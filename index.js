ddd2a49eab96d9e81c3bac025a61b0b467dc51f1
AboorvaDevarajan <aburvadevarajan@gmail.com>
2015-02-24 00:20:36 +0530

    Updated Chat module

--- index.js
+++ index.js
@@ -11,5 +11,15 @@ app.get("/",function(request, response){
 	response.render("page");
 });
 
-app.listen(port);
+app.use(express.static(__dirname+ '/resources'));
+
+var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port 3000...");
+
+io.sockets.on('connection' , function(socket) { 
+	socket.emit('message', { message : "Welcome to Jabber Out!"});
+	socket.on('send',function(data){
+		io.sockets.emit('message',data);
+	});
+});
+
