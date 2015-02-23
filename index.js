3441e6219051e50407e04fa3f53d710ebb434b0c
AboorvaDevarajan <aburvadevarajan@gmail.com>
2015-02-23 23:47:22 +0530

    Added Jade templates

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get("/",function(req, res){
-	res.send("It works!");
+app.set('views', __dirname + '/templates');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
+
+app.get("/",function(request, response){
+	response.render("page");
 });
 
 app.listen(port);
